5088f545d7ace403b2ac808f939df4763b2c5fc3
Estelle Michard <michardestelleem@gmail.com>
2025-04-07 16:00:00 +0200

    Commit de survie du Project_Green

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import Card from "./components/Card"
+import "./App.css"
+
+
+
+const arrayOfPlants = [
+  {
+    specie: "Brassicacées (Crucifères)",
+    imgSrc: "https://www.arcadie.fr/wp-content/uploads/2020/02/raifort-1-600x800.jpg",
+    description: "Le Raifort est une plante qui est utilisé en cuisine pour sa racine. Les Brassicacées sont aussi appelés de leur ancien nom Crucifère, en raison de leurs fleurs typiques à 4 pétales en croix."
+  },
+  {
+    specie: "Fabacées",
+    imgSrc: "https://www.arcadie.fr/wp-content/uploads/2023/06/pois-de-senteur-700x467.jpg",
+    description: "La famille des fabacées nous fournit tous les haricots, pois, fèves (appelés “légumes”). Elle est également appelée Papilionacées en raison de la forme de leurs fleurs évoquant un papillon (ailes)."
+  },
+  {
+    specie: "Lamiacées",
+    imgSrc: "https://www.arcadie.fr/wp-content/uploads/2023/06/1-576x1024.jpg",
+    description: "Les lamiacées sont une très grande famille de plantes médicinales : aucune des plantes de cette famille n'est toxique, quasiment toutes sont médicinales."
+  },
+]
+
+function App() {
+
+  return (
+    <>
+      <main>
+        {arrayOfPlants.map((plantes) => (
+          <Card key={plantes.specie} specie={plantes.specie} imgSrc={plantes.imgSrc} description={plantes.description} />
+        )
+        )}
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.tsx
@@ -0,0 +1,24 @@
+import "./Card.css"
+
+interface Plantes {
+    specie: string;
+    imgSrc: string;
+    description: string;
+}
+
+
+function Card({ specie, imgSrc, description }: Plantes) {
+
+    return (
+        <figure className="card">
+            <img src={imgSrc} alt={specie} />
+            <figcaption>{description}</figcaption>
+        </figure>
+    )
+}
+
+
+
+
+
+export default Card
