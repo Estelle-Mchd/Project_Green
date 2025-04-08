c488d868a8d553b6d36ef77ee61b4c8f7ff353af
Estelle Michard <michardestelleem@gmail.com>
2025-04-08 13:24:50 +0200

    Erreur corrigée sur Project_Green

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,14 +4,14 @@ interface Plantes {
     specie: string;
     imgSrc: string;
     description: string;
-    highlight: boolean;
+    highlight: boolean | string;
 }
 
 
-function Card({ specie, imgSrc, description }: Plantes) {
+function Card({ specie, imgSrc, description, highlight }: Plantes) {
 
     return (
-        <figure className={`card $highlight ? 'highlight' :''}`}>
+        <figure className={`card ${highlight ? 'highlight' : ''}`}>
             <img src={imgSrc} alt={specie} />
             <figcaption>{description}</figcaption>
         </figure>
